2a43f0e4f45bdfe2c063adcfe38fc8c93155dea2
Guilherme Bernardo <guilhermebernardo@id.uff.br>
2022-10-17 22:10:40 -0300

    feat: listagem de usuarios e meddlewares

---
+++ src/middlewares/ensureAdm.middleware.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction} from "express";
+
+const ensureAdmMiddleware = async() => {
+ 
+}
+
+export default ensureAdmMiddleware;
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,12 +1,15 @@
 import {Router} from 'express';
 import createUserController from '../controllers/users/createUser.controller';
+import listUsersController from '../controllers/users/listUsers.controller';
+import ensureAdmMiddleware from '../middlewares/ensureAdm.middleware';
+import ensureAuthMiddleware from '../middlewares/ensureAuth.middleware';
 
 //imports
 
 const routerUser = Router();
 
 routerUser.post('', createUserController);
-// routerUser.get('', "IMPORT");
+routerUser.get('', ensureAuthMiddleware, ensureAdmMiddleware, listUsersController);
 // routerUser.patch('/:id', "IMPORT");
 // routerUser.delete('/:id', "IMPORT");
 
