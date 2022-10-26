4d5af30a03d8759454bdeeb37e6c2c8b357769d8
Guilherme Bernardo <guilhermebernardo@id.uff.br>
2022-10-26 12:13:36 -0300

    feat: inclusão de testes para adicao de relacoes de novas tabelas ao db

--- src/__tests__/integration/users/userRoutes.test.ts
+++ src/__tests__/integration/users/userRoutes.test.ts
@@ -141,6 +141,21 @@ describe("/users", () => {
              
     })
 
+    test("PATCH /users/:id - should not be able to update user with invalid id",async () => {
+        const newValues = {name: "Joana Brito", email: "joanabrito@mail.com"}
+
+        const admingLoginResponse = await request(app).post("/login").send(mockedAdminLogin);
+        const token = `Bearer ${admingLoginResponse.body.token}`
+        
+        const userTobeUpdateRequest = await request(app).get("/users").set("Authorization", token)
+        const userTobeUpdateId = userTobeUpdateRequest.body[0].id
+
+        const response = await request(app).patch(`/users/13970660-5dbe-423a-9a9d-5c23b37943cf`).set("Authorization",token).send(newValues)
+
+        expect(response.body).toHaveProperty("message")
+        expect(response.status).toBe(404)
+    })
+
     test("PATCH /users/:id - should not be able to update isAdm field value",async () => {
         const newValues = {isAdm: false}
 
@@ -216,5 +231,6 @@ describe("/users", () => {
 
         expect(response.status).toBe(200)
         expect(userUpdated.body[0].name).toEqual("Joana Brito")
+        expect(userUpdated.body[0]).not.toHaveProperty("password")
     })    
 })
